707d4bb619887d9d376344b5b297095524d237e1
rahulmhjn <rahul.mahajan676@gmail.com>
2020-10-03 00:26:05 +0530

    express setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("E-commerce api");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, (req, res) => {
+  console.log("Server started");
+});
