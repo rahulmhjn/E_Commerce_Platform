44c6fc905bf6bbe5ed234819639b49c7095d130a
rahulmhjn <rahul.mahajan676@gmail.com>
2020-10-04 01:37:19 +0530

    Model Data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -24,5 +28,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, (req, res) => {
   console.log(
     `Server Started in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   );
 });
