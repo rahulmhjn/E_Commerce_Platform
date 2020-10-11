4a006c179c1bd56b975bc7dc666b608c689f444f
rahulmhjn <rahul.mahajan676@gmail.com>
2020-10-11 22:48:08 +0530

    User login reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,26 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage }, // if we close the window or refresh then the items are being loaded from the localstorage as soon as the store is initialized
+  userLogin: {userInfo: userInfoFromStorage},
 };
 
 const middleware = [thunk];
