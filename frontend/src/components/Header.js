d5431f64129c465dd10aa27299df9305c734b0c2
rahulmhjn <rahul.mahajan676@gmail.com>
2022-01-05 00:25:22 +0530

    product branding

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { logout } from "../actions/userActions";
-
+import Logo from "./Logo.png";
 const Header = () => {
   const dispatch = useDispatch();
 
@@ -18,7 +18,9 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>ProShop</Navbar.Brand>
+            <Navbar.Brand>
+              <img src={Logo} height="45vh" width="100vh" alt="Shop Gateway" />
+            </Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
