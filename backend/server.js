f36fdb7c7003ba2ce04d21cf714a5e7edab3e45d
rahulmhjn <rahul.mahajan676@gmail.com>
2022-01-12 23:17:26 +0530

    add logger: morgan

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -17,6 +18,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", UserRoutes);
