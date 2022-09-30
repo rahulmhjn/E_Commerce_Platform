00662a536293bb426b4b0786d2b2fb82ae0eee92
rahulmhjn <rahul.mahajan676@gmail.com>
2022-10-01 01:23:06 +0530

    Top Product Carousel

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -8,11 +8,13 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductsById)
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import Paginate from "../components/Paginate";
 import { listProducts } from "../actions/productActions";
+import ProductCarousel from "../components/ProductCarousel";
 
 const HomeScreen = ({ match }) => {
   const dispatch = useDispatch();
@@ -21,6 +22,7 @@ const HomeScreen = ({ match }) => {
 
   return (
     <React.Fragment>
+      {!keyword && <ProductCarousel />}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
